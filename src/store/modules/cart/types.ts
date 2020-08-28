43cb628061193134cae4e518010d9b0525907bf7
seveshy <danielalvesmajor@outlook.com>
2020-08-28 08:34:07 -0300

    Estado inicial do Reducer

---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+    id: number;
+    title: string;
+    price: number;
+}
+export interface ICartItem {
+    product: IProduct;
+    quantify: number;
+}
+
+export interface ICartState {
+    items: ICartItem[];
+}
